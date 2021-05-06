b5cd9765e2eba00e576b435553c19682d516ad47
die-goncalves <die.goncalves1990@gmail.com>
2021-05-06 16:10:49 -0300

    Add Cities component

---
+++ src/components/Cities/index.tsx
@@ -0,0 +1,75 @@
+import {
+  Box,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { CircleFlag } from "react-circle-flags";
+
+type CityInformation = {
+  country: string;
+  city: string;
+  countryCode: string;
+  photography: string;
+}
+type CitiesProps = {
+  mostVisitedCities: Array<CityInformation>;
+}
+
+export default function Cities({ mostVisitedCities }: CitiesProps) {
+  return (
+    <SimpleGrid minChildWidth="16rem" spacing="1.8125rem">
+      {mostVisitedCities.map((city) => {
+        return (
+          <Box width="100%" borderRadius="base" overflow="hidden" key={city.city}>
+            <Image
+              width="100%"
+              height="10rem"
+              src={city.photography}
+              alt={city.city}
+              objectFit="cover"
+            />
+
+            <Box
+              paddingX="1.5rem"
+              paddingY="1.125rem"
+              borderTop="none"
+              borderWidth="1px"
+              borderColor="brand.half-orange"
+              background="white"
+            >
+              <Box display="flex" justifyContent="space-between">
+                <Stack direction="column" spacing="0.75rem">
+                  <Text
+                    fontFamily="Barlow"
+                    fontWeight="600"
+                    fontSize="1.25rem"
+                    lineHeight="1.5625rem"
+                    color="brand.dark-head-text"
+                  >
+                    {city.city}
+                  </Text>
+                  <Text
+                    fontFamily="Barlow"
+                    fontWeight="500"
+                    fontSize="1rem"
+                    lineHeight="1.625rem"
+                    color="brand.dark-info"
+                  >
+                    {city.country}
+                  </Text>
+                </Stack>
+                <CircleFlag
+                  width="30"
+                  height="30"
+                  countryCode={city.countryCode.toLowerCase()}
+                />
+              </Box>
+            </Box>
+          </Box>
+        );
+      })}
+    </SimpleGrid>
+  )
+}
